4dcf3541ff5ea4916b6db9e51828287964649030
Utsav Parmar <105381305+UtsavParmar@users.noreply.github.com>
2023-05-23 10:55:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Restaurant from './components/basics/Restaurant'
+
+
+
+const App = () => {
+  return  (
+    <Restaurant/>
+    
+  )
+ 
+  
+}
+
+export default App
